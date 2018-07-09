1abc4f2e4621e041e74a1dce5e04dc8f73f35eda
gusev <sasha19755791@gmail.com>
2018-07-10 01:09:58 +0300

    Add Router Example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
+import { Switch, Route, Router  } from 'react-router';
 import { withStyles } from '@material-ui/core/styles';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import LoginForm from './Components/LoginForm/LoginForm';
 import Common from './Styles/Common';
-import 'typeface-roboto';
 
 const styles = ({
   content: {
@@ -22,6 +23,29 @@ class App extends Component {
       <div>
         <div className={classes.content}>
           <Header />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/l">
+                  None
+                </Link>
+              </li>
+              <li>
+                <Link to="/Components/LoginForm">
+                  Footer
+                </Link>
+              </li>
+              <li>
+                <Link to="/Components">
+                  Header
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/Components/LoginForm" component={Footer} />
+            <Route path="/Components" component={Header} />
+          </Switch>
           <LoginForm />
         </div>
         <Footer />
