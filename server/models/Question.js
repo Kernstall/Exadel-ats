41f82e517ecdb61f09c83af918095d0ed8a0b5df
DmitryKotusev <dssa-1966@yandex.ru>
2018-07-10 13:17:00 +0300

    Replacement

---
+++ server/models/Question.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const question = mongoose.Schema({
+  creatorId: mongoose.Schema.Types.ObjectId,
+  topicId: mongoose.Schema.Types.ObjectId,
+  tags: [String],
+  description: String,
+  correctAnswersIndexes: [Number],
+  answersVariants: [String],
+  kind: {
+    type: String,
+    enum: ['one answer', 'multiple answers', 'without answer option', 'without answer with verification'],
+  },
+  correntAnswersCount: Number,
+  wrongAnswersCount: Number,
+  difficultyRate: Number,
+  isTraining: Boolean,
+  isBlocked: Boolean,
+  haveCheckedReport: Boolean,
+});
+
+module.exports = mongoose.model('question', question);
--- server/models/TopicCourse.js
+++ server/models/TopicCourse.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const topicCourse = new mongoose.Schema({
-    name: String,
-    topicsIds: [mongoose.Schema.Types.ObjectId],
+  name: String,
+  topicsIds: [mongoose.Schema.Types.ObjectId],
 });
 
 module.exports = mongoose.model('topicCourse', topicCourse);
