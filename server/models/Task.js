e94146b1c1e1810a4ca5cc679d7aa0a7f28c9246
DmitryKotusev <dssa-1966@yandex.ru>
2018-07-10 13:08:22 +0300

    Some fixes

---
+++ server/models/Task.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const task = mongoose.Schema({
+  description: String,
+  name: {
+    type: String,
+    unique: true,
+  },
+  weight: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
+  topicsIds: [mongoose.Schema.Types.ObjectId],
+  language: String,
+  tags: [String],
+  tests:
+    [
+      {
+        inputFileAdress: String,
+        outputFileAdress: String,
+        weight: {
+          type: Number,
+          min: 1,
+          max: 10,
+        },
+      }],
+  passResult: Number,
+});
+
+module.exports = mongoose.model('task', task);
