af640f55a0925a7be376fe3a58e313d42f21d1f4
Prischep <ya.pryshchep@yandex.by>
2018-07-11 15:39:21 +0300

    add requests getStudentTasks, addTask, addQuestion

--- server/app.js
+++ server/app.js
@@ -2,15 +2,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
+const fs = require('fs');
 // const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const mongoose = require('mongoose');
+const studentRouter = require('./routes/student-router');
 
-const app = express();
+
+const StudentModel = require('./models/Student');
+const TopicModel = require('./models/Topic');
+const QuestionModel = require('./models/Question');
 
 const dbName = 'TestingSystem';
-const connectionString = `mongodb://localhost:27017/${dbName}`;
-mongoose.connect(connectionString);
+mongoose.Promise = global.Promise;
+
+async function connectDatabase() {
+  mongoose.connect(`mongodb://localhost:27017/${dbName}`)
+    .then(() => {
+      console.log('Connected to database!!!');
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+}
+
+connectDatabase();
+
+let taskArray = [];
+
+QuestionModel.find()
+  .then((response) => {
+    taskArray = response;
+    fs.writeFileSync('./questions.json', JSON.stringify(taskArray));
+  });
+
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,6 +56,9 @@ app.use(session({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/api/student', studentRouter);
+
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
---
+++ server/dataFunctions.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Task = require('./models/Task');
+const Question = require('./models/Question');
+
+exports.getStudentTasks = function (taskArrayStud) {
+  const arrayTaskIds = [];
+  taskArrayStud.forEach((elem) => {
+    arrayTaskIds.push(elem.taskId);
+  });
+
+  return Task.find()
+    .where('_id')
+    .in(arrayTaskIds);
+};
+
+exports.addTask = function (req) {
+  const task = new Task({
+    description: req.description,
+    name: req.name,
+    weight: req.weight,
+    topicId: req.topicId,
+    language: req.language,
+    tags: req.tags,
+    tests: req.tests,
+    passResult: req.passResult,
+  });
+  return task.save();
+};
+
+exports.addQuestion = function (req) {
+  const question = new Question({
+    creatorId: req.creatorId,
+    topicId: req.topicId,
+    tags: req.tags,
+    description: req.description,
+    correctAnswersIndexes: req.correctAnswersIndexes,
+    answersVariants: req.answersVariants,
+    kind: req.kind,
+    correntAnswersCount: req.correntAnswersCount,
+    wrongAnswersCount: req.wrongAnswersCount,
+    difficultyRate: req.difficultyRate,
+    isTraining: req.isTraining,
+    isBlocked: req.isBlocked,
+    haveCheckedReport: req.haveCheckedReport,
+  });
+  return question.save();
+};
