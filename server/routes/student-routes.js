7295b6eb5bfbcbc0ac2a70416bef4b911b8c9fab
Yana Sidorova <jana.ru.sidorova@yandex.ru>
2018-07-12 12:40:24 +0300

    edit tasks

--- server/routes/student-routes.js
+++ server/routes/student-routes.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const Student = require('../models/Student');
 const Group = require('../models/Group');
+const Employee = require('../models/Employee');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // find by id
-  Promise.all([
-    Student.findById({ _id: '5b45b16d75224332745f758e' }),
-    Group.find({ studentIdList: '5b45b16d75224332745f758e' }),
-  ])
-    .then((result) => {
-      if (!result[0] || !result[1]) {
-        return Promise.reject();
-      }
-      const studentInfo = [{
-        firstName: result[0].firstName,
-        lastName: result[0].lastName,
-        faculty: result[0].faculty,
-        course: result[0].faculty,
-        groupNumber: result[0].groupNumber,
-        graduateYear: result[0].graduateYear,
-      }, result[1]];
-      res.send(studentInfo);
-    })
-    .catch(err => res.status(500).send(err));
+router.get('/', (req, res) => {
+  if (req.query.id) {
+    Promise.all([
+      Student.findById({ _id: req.query.id }),
+      Group.find({ studentIdList: req.query.id }),
+    ])
+      .then((result) => {
+        if (!result[0] || !result[1]) {
+          return Promise.reject();
+        }
+        const studentInfo = [{
+          firstName: result[0].firstName,
+          lastName: result[0].lastName,
+          faculty: result[0].faculty,
+          course: result[0].faculty,
+          groupNumber: result[0].groupNumber,
+          graduateYear: result[0].graduateYear,
+        }, result[1]];
+        Promise.resolve(studentInfo);
+      })
+      .then((result) => {
+        Employee.findById({ _id: result[1].teacherId }, (tres) => {
+          Promise.resolve(tres);
+        });
+      })
+      .catch(err => res.status(500).send(err));
+  } else res.status(400).end();
 });
 
 module.exports = router;
