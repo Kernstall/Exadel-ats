36d71c380e45656acd5505a66490dae32a09d369
DmitryKotusev <dssa-1966@yandex.ru>
2018-07-13 16:35:52 +0300

    Added fields to users and upgraded groups model

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -1,4 +1,10 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const Student = require('../models/User');
 
 const router = express.Router();
+
+router.get('/Tops', async (req, res) => {
+  const result = {};
+  result.tasksTop = await Student.find({}).limit(10);
+});
