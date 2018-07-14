784a7251d79478d7d17c0ec8a2bc850b82525082
DmitryKotusev <dssa-1966@yandex.ru>
2018-07-15 01:59:25 +0300

    Done 3 of 4 top requests

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -7,21 +7,65 @@ const router = express.Router();
 
 router.get('/Tops', async (req, res) => {
   const result = {};
-  result.tasksTop = await Student.find({}, { firstName: true, lastName: 1, mediumTaskScore: 1 })
+  result.tasksTop = await Student.find({ status: 'student' }, { firstName: true, lastName: 1, mediumTaskScore: 1 })
     .sort({ mediumTaskScore: -1 }).limit(10);
   const studentTaskFields = ['firstName', 'lastName', 'mediumTaskScore', 'id'];
   result.tasksTop = dataFunctions.fieldFilter(studentTaskFields, result.tasksTop);
-  /*console.log(result.tasksTop[0]._id);
-  console.log(result.tasksTop[0].id);
-  console.log(result.tasksTop[0]);*/
-  result.testsTop = await Student.find({}, { firstName: 1, lastName: 1, mediumTestScore: 1 })
+
+  result.testsTop = await Student.find({ status: 'student' }, { firstName: 1, lastName: 1, mediumTestScore: 1 })
     .sort({ mediumTestScore: -1 }).limit(10);
   const studentTestFields = ['firstName', 'lastName', 'mediumTestScore', 'id'];
   result.testsTop = dataFunctions.fieldFilter(studentTestFields, result.testsTop);
-  /*result.testsTop = result.testsTop.map((student) => studentFields.reduce((obj, key) => {
-    obj[key] = student[key]
-    return obj;
-  }, {}));*/
+
+  result.activitiesTop = await Student.aggregate([
+    { $match: { status: 'student' } },
+    {
+      $project: {
+        activity: {
+          $let: {
+            vars: {
+              taskActivity: {
+                $reduce: {
+                  input: '$tasks',
+                  initialValue: 0,
+                  in: { $add: ['$$value', { $size: '$$this.attempts' }] },
+                },
+              },
+              testActivity: {
+                $reduce: {
+                  input: '$tests',
+                  initialValue: 0,
+                  in: {
+                    $cond: {
+                      if: {
+                        $ne: ['$$this.status', 'notSent'],
+                      },
+                      then: {
+                        $add: ['$$value', 1],
+                      },
+                      else: {
+                        $add: ['$$value', 0],
+                      },
+                    },
+                  },
+                },
+              },
+            }, // end of vars
+            in: { $add: ['$$taskActivity', '$$testActivity'] },
+          },
+        }, // end of activity field
+        id: true,
+        firstName: true,
+        lastName: true,
+      },
+    },
+    {
+      $sort: { activity: -1 },
+    },
+    {
+      $limit: 10,
+    },
+  ]).allowDiskUse(true);
 
   res.send(JSON.stringify(result));
 });
