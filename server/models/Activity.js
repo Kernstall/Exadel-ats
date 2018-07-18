47312a7b1b68eab172e1a65a8466daf3a0bccfbd
gusev <sasha19755791@gmail.com>
2018-07-18 17:04:46 +0300

    init admin main page and some other configuration

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -4,10 +4,10 @@ const ActivitiesSchema = new mongoose.Schema({
   type: {
     type: String,
     enum: ['1)studentGroupAddition', '2)studentGroupRemove', '3)groupCreation', '4)studentTaskAssignment', '5)groupTaskAssignment',
-      '6)studentTestAssignment', '7)studentTestAssignment', '8)studentTestSending', '9)studentTestComplete', '10)teacherTestCheck',
+      '6)groupTestAssignment', '7)studentTestAssignment', '8)studentTaskSending', '9)studentTestComplete', '10)teacherTestCheck',
       '11)studentQuestionComplaint', '12)teacherQuestionCreation', '13)adminQuestionCreation', '14)teacherTaskCreation',
-      '15)adminTaskCreation', '16)teacherTaskBlock', '17)adminTaskBlock', '18)teacherRightsToStudentDelegation',
-      '19)adminRightsToStudentDelegation', '20)adminRightsToTeacherTransfer'],
+      '15)adminTaskCreation', '16)teacherQuestionBlock', '17)adminQuestionBlock', '18)teacherRightsToStudentDelegation',
+      '19)adminRightsToStudentDelegation', '20)adminRightsToTeacherDelegation'],
   },
   userType: {
     type: String,
