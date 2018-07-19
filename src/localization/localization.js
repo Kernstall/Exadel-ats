bb81a6bad96692535f628971af4aa3794f4071ba
gusev <sasha19755791@gmail.com>
2018-07-20 01:42:52 +0300

    Add localization

---
+++ src/localization/localization.js
@@ -0,0 +1,52 @@
+const localize = (keyWord) => {
+  const spnTanslations = {};
+  const engTranslations = {};
+  const ruTranslations = {
+    teacher: 'Учитель',
+    student: 'Студент',
+    admin: 'Администратор',
+    studentGroupAddition: 'Студент добавлен в группу',
+    studentGroupRemove: 'Студент удален из группы',
+    groupCreation: 'Добавлена группа студентов',
+    studentTaskAssignment: 'Назначена задача студенту',
+    groupTaskAssignment: 'Назначена задача группе',
+    studentTestAssignment: 'Назначен тест студенту',
+    groupTestAssignment: 'Назначен тест группе',
+    studentTaskSending: 'Стуент отправил решение задачи',
+    studentTestComplete: 'Студент прошел тест',
+    teacherTestCheck: 'Учитель проверил тест',
+    studentQuestionComplaint: 'Студент пожаловался на вопрос',
+    teacherQuestionCreation: 'Учитель создал вопрос',
+    adminQuestionCreation: 'Администратор создал вопрос',
+    teacherTaskCreation: 'Учитель создал задачу',
+    adminTaskCreation: 'Администратор создал задачу',
+    teacherQuestionBlock: 'Учитель заблокировал вопрос',
+    adminQuestionBlock: 'Администратор заблокировал вопрос',
+    teacherRightsToStudentDelegation: 'Студенту переданы права учителя',
+    adminRightsToStudentDelegation: 'Студенту переданы права администратора',
+    adminRightsToTeacherDelegation: 'Учителю переданы права администратора',
+  };
+
+  const dictionaries = {
+    ru: ruTranslations,
+    'ru-RU': ruTranslations,
+    en: engTranslations,
+    spn: spnTanslations,
+  };
+
+  const langToWords = (lang) => {
+    console.log(lang());
+    console.log(dictionaries[lang()]);
+    return dictionaries[lang()] || null;
+  };
+
+
+  function findOutLang() {
+    return window.navigator.language;
+  }
+
+  const currentDictionary = langToWords(findOutLang);
+  return currentDictionary ? currentDictionary[keyWord] || keyWord : keyWord;
+};
+
+export default localize;
