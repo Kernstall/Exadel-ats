0a096316739c9258c4b334324f96b6cf142c7aba
Raman Buyak <xjordward@gmail.com>
2018-07-20 17:13:33 +0300

    teacher create group wip

--- src/common/teacherAddGroup/TeacherAddGroup.jsx
+++ src/common/teacherAddGroup/TeacherAddGroup.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import FilterStudentCard from './FilterFieldsCard.jsx';
 import DragAndDropStudents from './DragAndDropStudents';
 import { getAvailableStudents } from '../../commands/teacherCreateGroups';
-import Spinner from "../shared/spinner";
+import Spinner from '../shared/spinner';
 
 const styles = {
   FlexContainerHorizontal: {
@@ -15,16 +15,51 @@ const styles = {
 };
 
 class TeacherAddGroup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filter: {
+        name: '',
+        email: '',
+        university: '',
+        faculty: '',
+        year: undefined,
+      },
+    };
+
+    this.handleFilterChanges = this.handleFilterChanges.bind(this);
+  }
+
   componentDidMount() {
     this.props.getAvailableStudents(/*{ Filter will be here }*/); // eslint-disable-line
   }
 
+  handleFilterChanges(filterObject) {
+    console.log(filterObject);
+    this.setState({
+      filter: filterObject,
+    });
+  }
+
   render() {
-    const { classes } = this.props;
+    const { classes, teacher_AvailableStudentsList } = this.props;
+    let filteredArray;
+    if (teacher_AvailableStudentsList) {
+      filteredArray = teacher_AvailableStudentsList.filter((element) => {
+        const fName = (element.firstName + ' ' + element.lastName).indexOf(this.state.filter.name) >= 0;
+        const fEmail = element.email.indexOf(this.state.filter.email) >= 0;
+        const fUnivesity = element.university === this.state.filter.university || this.state.filter.university === ' ';
+        const fYear = element.graduateYear === this.state.filter.year || this.state.filter.year === undefined;
+        const fFaculty = element.faculty === this.state.filter.faculty || this.state.filter.faculty === ' ';
+        return fName && fEmail;
+      });
+    } else {
+      filteredArray = [];
+    }
     return (
       <div className={classes.FlexContainerHorizontal}>
-        <FilterStudentCard />
-        {this.props.teacher_AvailableStudentsList ? <DragAndDropStudents studentsPool={this.props.teacher_AvailableStudentsList} /> : <Spinner /> }
+        <FilterStudentCard callback={this.handleFilterChanges} />
+        {teacher_AvailableStudentsList ? <DragAndDropStudents studentsPool={filteredArray} /> : <Spinner /> }
       </div>
     );
   }
