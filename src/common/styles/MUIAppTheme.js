b85f9988f0af33798e8576ca0f3961632eb9e455
Raman Buyak <xjordward@gmail.com>
2018-07-31 16:12:27 +0300

    custom palette  added, create test question component started

--- src/common/styles/MUIAppTheme.js
+++ src/common/styles/MUIAppTheme.js
@@ -1,11 +1,27 @@
-import { green, red, blue, cyan } from '@material-ui/core/colors';
+import {
+  blue,
+  red,
+} from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export default createMuiTheme({
   palette: {
-    primary: cyan,
-    secondary: green,
-    surface: blue,
+    primary: blue,
+    custom: {
+      blackText: '#212121',
+      grayText: '#757575',
+      whiteText: '#FFFFFF',
+      grayDivider: '#BDBDBD',
+      dark: '#28639d',
+      primary: '#1976d2',
+      light: '#90caf9',
+      accent: '#FFC107',
+      background: '#d3d3d3',
+      juicy: '#ff9100',
+      juicySoft: '#ffab40',
+    },
+    secondary: red,
+    background: red,
     error: red,
   },
 });
