e7ce8506936722e54a35f9850b05a4855a52c20f
Prischep <ya.pryshchep@yandex.by>
2018-07-31 12:01:17 +0300

    send files

--- server/routes/teacher-router.js
+++ server/routes/teacher-router.js
@@ -15,12 +15,12 @@ const fileSystemFunctions = require('../utils/fileSystemFunctions.js');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
+/*router.use((req, res, next) => {
   if (req.user.status !== 'teacher') {
     return res.status(403).end();
   }
   return next();
-});
+});*/
 
 router.get('/tasks', (req, res) => {
   let hashSet = {};
