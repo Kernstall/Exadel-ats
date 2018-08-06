ee7179867a5402753e9413fcb15119cf258b2c95
Prischep <ya.pryshchep@yandex.by>
2018-08-06 16:36:48 +0300

    fix catch

--- server/routes/student-router.js
+++ server/routes/student-router.js
@@ -143,7 +143,6 @@ router.post('/src/files', uploadFiles.uploadSrcCode.array('src'), async (req, re
     res.status(400).send(e.toString());
   }
 });
-
 router.get('/test/questions', async (req, res) => {
   try {
     const topicId = req.query.topicId;
