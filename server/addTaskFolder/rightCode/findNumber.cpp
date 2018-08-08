2112b39557662744d002e77a272be4e77b61f674
yana617 <31696608+yana617@users.noreply.github.com>
2018-08-08 17:50:01 +0300

    Update findNumber.cpp

--- server/addTaskFolder/rightCode/findNumber.cpp
+++ server/addTaskFolder/rightCode/findNumber.cpp
@@ -4,17 +4,14 @@
 using namespace std;
 
 int main() {
-	//ifstream in("input.txt");
-	//ofstream out("output.txt");
-	int num = 1000506, n = 8, k = let(num);
-	//in >> num >> n;
-
-	if(k < n) cout << -1;
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	int num, n; 
+	in >> num >> n;
+        int k = let(num);
+	if(k < n) out << -1;
 	else {
-		cout << (num % (int)(pow(10, k - n + 1)) - num % (int)(pow(10, k - n))) / (int)(pow(10, k - n));
+		out << (num % (int)(pow(10, k - n + 1)) - num % (int)(pow(10, k - n))) / (int)(pow(10, k - n));
 	}
-
-
-	system("pause");
-	return 0;
+        return 0;
 }
