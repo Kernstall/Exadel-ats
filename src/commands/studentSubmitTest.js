f74a2c4bf4dbb44e83a511714695677f23c89aba
Raman Buyak <xjordward@gmail.com>
2018-08-08 11:30:08 +0300

    Redux part for прохождение теста

---
+++ src/commands/studentSubmitTest.js
@@ -0,0 +1,35 @@
+import { studentSubmitTest as Actions, errorMessage as errorEmmiter } from '../actions';
+
+export const submitTest = testObject => (dispatch) => {
+  dispatch(Actions.studentSubmitTestRequest(testObject));
+  fetch('Мой адрес запроса !!!', {
+    method: 'POST',
+    body: JSON.stringify({
+      ...testObject,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+      'Set-Cookie': 'true',
+    },
+    credentials: 'include',
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+      switch (response.status) {
+        case 409:
+          throw new Error('Не удалось послать тест на проверку');
+        default:
+          throw new Error('Не удалось послать тест на проверку');
+      }
+    })
+    .then((body) => {
+      dispatch(Actions.studentSubmitTestSuccess());
+      dispatch(errorEmmiter.messageRequested('Тест успешно отправлен на проверку', `Юрла для редиректа!!!!`));
+    })
+    .catch((err) => {
+      dispatch(Actions.studentSubmitTestError(err));
+      dispatch(errorEmmiter.messageRequested(err.message));
+    });
+};
